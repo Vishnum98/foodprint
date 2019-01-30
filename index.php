328f9d5f5b0558152242c2602328a7481f3a913d
unknown <vishnum1998@gmail.com>
2019-01-30 15:02:36 +0530

    hhomepage

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php require_once('config.php') ?>
+<?php require_once( ROOT_PATH . '/includes/public_functions.php') ?>
+
+<!-- Retrieve all posts from database  -->
+<?php $posts = getPublishedPosts(); ?>
+<?php require_once( ROOT_PATH . '/includes/head.php') ?>
+	<title>Food Print </title>
+</head>
+<body>
+	<!-- container - wraps whole page -->
+<div class="container"><div><span class="align-right"> 
+ 	<a class="btn btn-primary" href="#" role="button">Login</a></span></div>	
+<section id="introduction">  
+ 
+   <div class="row fill-viewport align-items-center">
+     <div class="col-12 col-md-6">
+ 
+       <h1 class="text-white">You don't waste just a plate of food</h1>
+       <p class="lead text-white-70">Lame Text  </p>
+      <section class="columns lead text-white-70">
+	
+	<div class="column">
+    
+    <h4  id="number">0</h4> <h4>litres</h4>
+		<p>of water could have been saved  </p>
+	</div>
+	
+	<div class="column">
+		<h4>10 Calories</h4>
+		<p>could have been fed </p>
+	</div>
+  
+  <div class="column">
+		<h4 id="number">10 acre</h4>
+		<p>land could have been used anywhere</p>
+	</div>
+	
+</section>	
+	
+     </div>
+   </div>
+ </div>
+</section>
+
+
+</body>
+</html>
